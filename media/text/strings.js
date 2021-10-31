8a638d3ac713bbc223a975b75b4ae43424a8f3c1
Song Wook <songtri@gmail.com>
2021-10-31 11:55:32 -0400

    upload game

---
+++ media/text/strings.js
@@ -0,0 +1,57 @@
+var _STRINGS = {
+	"Ad":{
+		"Mobile":{
+			"Preroll":{
+				"ReadyIn":"The game is ready in ",
+				"Loading":"Your game is loading...",
+				"Close":"Close",
+			},
+			"Header":{
+				"ReadyIn":"The game is ready in ",
+				"Loading":"Your game is loading...",
+				"Close":"Close",
+			},
+			"End":{
+				"ReadyIn":"Advertisement ends in ",
+				"Loading":"Please wait ...",
+				"Close":"Close",
+			},								
+		},
+	},
+	
+	"Splash":{
+		"Loading":"Loading ...",	
+		"LogoLine1":"Some text here",
+		"LogoLine2":"powered by MarketJS",
+		"LogoLine3":"none",		
+        "TapToStart":"TAP TO START",				
+	},
+
+	"Game":{
+		"SelectPlayer":"Select Player",
+		"Win":"You win!",
+		"Lose":"You lose!",
+		"Score":"Score",
+		"Time":"Time",
+		"Shield":"SHIELD",
+		"Magnet": "COIN MAGNET",
+		"Ammo":"AMMO",
+		"Next":"NEXT",
+	},
+
+	"Results":{
+		"Title":"High score",
+	},
+
+	"Tutorial":{
+		"Obstacle": "Alert! Incoming obstacles.",
+		"MovementDesktop":"Use left or right arrow keys to move your boat.",
+		"MovementMobile":"Tap/hold left or right to move your boat.",
+		"ShootDesktop":"Hit Spacebar to destroy obstacles.",
+		"ShootMobile":"Shoot to destroy obstacles.",
+		"CollectCoin":"Collect coins to buy upgrades.",
+		"PowerUp":"Collect powerups to make your boat stronger.",
+		"Ramp":"Use the ramp for an extra boost.",
+		"End":"This concludes the tutorial. Good luck!"
+	},
+};
